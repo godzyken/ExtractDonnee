c9e834681f372c9a19bc606fac26037ff6f3248f
godzyken <isgodzy@msn.com>
2019-01-14 00:34:51 +0100

    extraire donnee en cours
    Signed-off-by: godzyken <isgodzy@msn.com>

--- bronze/Bronze.js
+++ bronze/Bronze.js
@@ -38,5 +38,7 @@ var Bronze = {
 };
 
 var extractDonnee = require('../extractDonnee');
+var readFirstSheet = require('../').readFirstSheet;
+console.log(readFirstSheet("a,b,c\n1,2,3\n4,5,6", {type: "binary"}));
 
 module.exports = Bronze;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var app = require('./app');
-var port = process.env.PORT || 3006;
+var port = process.env.PORT || 3000;
 var server = app.listen(port, function () {
     console.log('Express server en lecture du port ' + port);
     const all_routes = require('express-list-endpoints');
